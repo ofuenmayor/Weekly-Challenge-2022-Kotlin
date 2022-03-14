302826c19a72a96093e3d3fba77ae62510f6c247
Omar Fuenmayor <omarfuenmayorroo@gmail.com>
2022-03-14 04:48:16 -0700

    feat(init project): preparing for making challenge

---
+++ test/challenge0.ts
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+
+describe('Challenge 0 Fizz ',()=>{
+	it('when recibe a multiple of 3 get fizz', () => {
+
+	});
+	it('when recibe a multiple of 5 get buzz', () => {
+
+	});
+	it('when recibe a multiple of 3 and 5 get fizz buzz', () => {
+
+	});
+});
